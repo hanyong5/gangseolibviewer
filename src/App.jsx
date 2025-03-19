af044c9cbae80ef79fd051b0ba659e959c9f2891
hanyong5 <hanyong5@naver.com>
2025-03-19 23:34:56 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import Wave from "react-wavify";
+
+function App() {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  const [isListening, setIsListening] = useState(false);
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <div>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <button
+        onClick={SpeechRecognition.startListening}
+        className="block border-2 px-1"
+      >
+        Start
+      </button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  );
+}
+
+export default App;
